d52181bd9c6f4b3be0df3f84846b113fb751c01e
aleksandr <digitalpochta@mail.ru>
2022-08-29 16:37:09 +0300

    add file 1stFile

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-# This is a sample Python script.
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+print("My name is {}".format("Elias"))
+my_name = "John"
+print("My name is {}".format(my_name))
+name = "Jon"
+age = 30
+print(f"My name is {name} and I'm {age}")
 
+dirpath = os.getcwd()
+print(f"Path is {dirpath}")
+
+#my_file = open("1stFile.txt", "a+")
+#my_file.write("\nDoe")
+#my_file.close()
+#print(type(my_file))
+
+with open('1stFile.txt', 'a+') as forRead_file:
+    #forRead_file.write('\nabraCadabra')
+    forRead_file.seek(0)
+    dataStr = forRead_file.read()
+    forRead_file.seek(0)
+    dataList = forRead_file.readlines()
+    print(type(dataList))
+    print(dataList)
+    forRead_file.seek(0)
+    print("file dataStr: " + dataStr)
+    print(forRead_file.read())
 
 
