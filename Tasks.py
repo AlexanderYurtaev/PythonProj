363520f528765a5412f1b523185dd22d370e1f8f
aleksandr <digitalpochta@mail.ru>
2022-09-27 17:21:56 +0300

    task1 completed

--- Tasks.py
+++ Tasks.py
@@ -5,3 +5,19 @@
 #
 # Подсказка: для "загадывания" числа используйте модуль random: импортируйте его и для генерации (загадывания)
 # числа вызовите метод random.randint(1, 50).
+import random
+
+generatedNumber = random.randint(1, 5)
+print(generatedNumber)
+#print(type(generatedNumber))
+
+for i in range(0, 6):
+    input_digit = int(input())
+    #print(type(input_digit))
+    if input_digit == generatedNumber:
+        print(f'Congratulations. You guessed the number {input_digit}.')
+        break
+    elif i == 5:
+        print(f'Number is not correct. The game is over.')
+    else:
+        print(f'Number is not correct. Please choose another number. {5 - i} trying left.')
