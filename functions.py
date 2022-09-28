80db4ed1d6ebf841d571e4badf0cdbd1502a20b9
aleksandr <digitalpochta@mail.ru>
2022-09-28 17:57:18 +0300

    added file functions

---
+++ functions.py
@@ -0,0 +1,23 @@
+# embedded functions
+abs(-1)
+abs(1)
+max_number = max(1, 2, 3, 4, 5)
+print(f'Max number is {max_number}.')
+min_number = min(1, 2, 3, 4, 5)
+print(f'Min number is {min_number}.')
+h = hex(42)
+o = oct(42)
+b = bin(42)
+print(h)
+print(o)
+print(b)
+
+letters = 'abcd'
+numbers = (10, 20, 30)
+zipped = zip(letters, numbers)
+print(type(zipped))
+print(zipped)
+zipped_list = list(zipped)
+print(zipped_list)
+
+# basic functions
