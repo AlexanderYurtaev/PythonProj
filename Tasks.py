b0369ef64d9bffdff0e0ec511f37d3e0505382e6
aleksandr <digitalpochta@mail.ru>
2022-11-24 16:58:33 +0300

    wqqweqw

--- Tasks.py
+++ Tasks.py
@@ -9,11 +9,11 @@ import random
 
 generatedNumber = random.randint(1, 5)
 print(generatedNumber)
-#print(type(generatedNumber))
+# print(type(generatedNumber))
 
 for i in range(0, 6):
     input_digit = int(input())
-    #print(type(input_digit))
+    # print(type(input_digit))
     if input_digit == generatedNumber:
         print(f'Congratulations. You guessed the number {input_digit}.')
         break
@@ -21,3 +21,23 @@ for i in range(0, 6):
         print(f'Number is not correct. The game is over.')
     else:
         print(f'Number is not correct. Please choose another number. {5 - i} trying left.')
+
+# Даны три неубывающих массива чисел. Найти число, которое присутствует во всех трех массивах.
+# Input: [1,2,4,5], [3,3,4], [2,3,4,5,6]
+a_list = [1, 2, 4, 5]
+b_list = [3, 3, 4]
+c_list = [2, 3, 4, 5, 6]
+
+
+def findNumber(a, b, c):
+    answer_list = []
+    for i in a:
+        for i2 in b:
+            for i3 in c:
+                if i == i2 and i == i3:
+                    if i not in answer_list:
+                        answer_list.append(i)
+    return answer_list
+
+
+print(findNumber(a_list, b_list, c_list))
